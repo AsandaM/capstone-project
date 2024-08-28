e2c6971f28d4cb1376c2730860fb6bd599483246
AsandaM <asandamehlo088@gmail.com>
2024-08-28 15:05:20 +0200

    :construction:

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,8 @@ import axios from 'axios'
 
 export default createStore({
   state: {
-    products:null
+    products:null, 
+    cart: null
 
   },
   getters: {
@@ -14,12 +15,21 @@ export default createStore({
   mutations: {
     setProducts(state, payload){
       state.products = payload
+    },
+    setCart(state, payload){
+      state.cart = payload
     }
   },
   actions: {
    async getProducts({commit}){
       let {data} = await axios.get('http://localhost:5005/products')
       commit('setProducts', data)
+      console.log(data);
+      
+    },
+   async addToCart({commit}, prodID){
+      let {data} = await axios.post('http://localhost:5005/products/cart', {prodID})
+      commit('setCart', data)
       console.log(data);
       
     }
