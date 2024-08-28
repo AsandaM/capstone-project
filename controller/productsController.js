56a227df146b89bcb9e7f2c54dfb37c049a3c2c6
AsandaM <asandamehlo088@gmail.com>
2024-08-28 10:11:29 +0200

    :card_file_box:

---
+++ controller/productsController.js
@@ -0,0 +1,87 @@
+import {getProductsDb, getProductDb, deleteProductDb, insertProductDb, editProductDb} from '../model/productsDb.js'
+
+// get all products
+const getProducts = async(req, res)=>{
+    try{
+        res.status(200).json(await getProductsDb())
+    } catch(err){
+        res.status(500).send('Error fetching products')
+        throw err
+        
+    }
+}
+
+// get single product by id
+const getProduct = async(req, res)=>{
+    try {
+        res.status(200).json(await getProductDb(req.params.id))
+        
+    } catch (err) {
+        res.status(500).send('Error fetching  a single product')
+        throw err
+    }
+}
+
+// insert product
+
+const insertProduct = async(req, res)=>{
+    let {prodName, price, prodDesc, image, category, quantity, ingredients, usage_instructions} = req.body
+
+    try {
+        await insertProductDb(prodName, price, prodDesc, image, category, quantity, ingredients, usage_instructions)
+        res.status(200).json(await getProductDb())
+        
+    } catch (err) {
+        res.status(500).send('Error inserting a product')
+        throw err
+        
+    }
+}
+
+// delete product
+
+const deleteProduct = async(req, res)=>{
+    try {
+        await deleteProductDb(req.params.id)
+        res.status(200).json(await getProductsDb())
+        
+    } catch (err) {
+        res.status(500).send('Error deleting a products')
+        throw err
+    }
+}
+
+// update product
+const editProduct = async(req, res)=>{
+    let {prodName, price, prodDesc, image, category, quantity, ingredients, usage_instructions} = req.body
+    let product = await getProductDb(req.params.id)
+
+    console.log(product);
+    
+
+    if(!product){
+        res.status(404).send('Product not found')
+    }
+    
+    prodName? prodName = prodName: prodName = product.prodName
+    quantity? quantity = quantity: quantity = product.quantity
+    price? price = price: price = product.price
+    category? category = category: category = product.category
+    image? image = image: image = product.image
+    prodDesc? prodDesc = prodDesc: prodDesc = product.prodDesc
+    ingredients? ingredients = ingredients: ingredients = product.ingredients
+    usage_instructions? usage_instructions = usage_instructions: usage_instructions = product.usage_instructions
+
+    try {
+        await editProductDb(prodName, price, prodDesc, image, category, quantity, ingredients, usage_instructions, req.params.id)    
+        res.status(200).send(await getProductsDb())
+    } catch (error) {
+        res.status(500).send('Error editing a product')
+        throw err
+    }
+    
+
+}
+
+
+export {getProducts, getProduct, insertProduct, deleteProduct, editProduct}
---
+++ model/usersDb.js
@@ -0,0 +1,40 @@
+import {pool} from '../config/config.js'
+
+const getUsersDb = async()=>{
+    let [data] = await pool.query('SELECT * FROM users')
+    return data
+}
+
+const getUserDb = async(id)=>{
+    let [[data]] = await pool.query('SELECT * FROM users WHERE userID = ?', [id])
+    return data
+}
+
+const loginDb = async(emailAdd)=>{
+    let [[data]] = await pool.query('SELECT * FROM users WHERE emailAdd = ?', [emailAdd])
+    return data
+}
+
+const deleteUserDb = async(id)=>{
+    await pool.query('DELETE FROM users WHERE userID = ?', [id])
+}
+
+const getUserByEmailDb = async (email) => {
+    const [user] = await pool.query('SELECT * FROM users WHERE emailAdd = ?', [email]);
+    return user.length > 0 ? user[0] : null;
+   
+}
+
+const insertUserDb = async(firstName, lastName, userAge, gender, emailAdd, userPass, userProfile)=>{
+    let [data] = await pool.query(`INSERT INTO users (firstName, lastName, userAge, gender, emailAdd, userPass, userProfile) VALUES (?,?,?,?,?,?,?)`, [firstName, lastName, userAge, gender, emailAdd, userPass, userProfile])
+    return data
+}
+
+
+const editUserDb = async(firstName, lastName, userAge, gender, emailAdd, userPass, userProfile, id)=>{
+    await pool.query(`UPDATE users 
+        SET firstName = ?, lastName = ?, userAge = ?, gender = ?, emailAdd = ? , userPass = ?, userProfile = ?
+        WHERE userID = ?`, [firstName, lastName, userAge, gender, emailAdd, userPass, userProfile, id])
+}
+
+export {getUsersDb, getUserDb, deleteUserDb, insertUserDb, editUserDb, loginDb, getUserByEmailDb}
